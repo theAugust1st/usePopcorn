8fbc96e1a9d1e7bf364872d60eb7450cbd1ab238
theAugust1st <bhote666@gmail.com>
2025-06-16 12:46:22 +0545

    fix: use https star svg

--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -63,7 +63,7 @@ function Star({ onRate, full, onEnter, onLeave,color,size }) {
     >
       {full ? (
         <svg
-          xmlns="http://www.w3.org/2000/svg"
+          xmlns="https://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
           fill={color}
           stroke={color}
@@ -72,7 +72,7 @@ function Star({ onRate, full, onEnter, onLeave,color,size }) {
         </svg>
       ) : (
         <svg
-          xmlns="http://www.w3.org/2000/svg"
+          xmlns="https://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
           stroke={color}
